5f9b4b62034b1d05855d60e2d0af6a104861b29f
L-Ray <lennart_reuther@web.de>
2014-01-17 19:22:06 +0000

    using modular structure

--- test/simmetrix.smithwaterman.js
+++ test/simmetrix.smithwaterman.js
@@ -1,18 +1,18 @@
-require('../lib/simmetrix.smithwaterman.js')
+var smithWaterman = require('../lib/simmetrix.smithwaterman.js')
 var expect = require('expect.js');
 
 describe('Simmetrix.SmithWaterman', function(){
   describe('#similarity()', function(){
     it('should return a proper similarity between 0 and 1', function(){
-      expect(Simmetrix.SmithWaterman.similarity("Test String1", "Test String2").toFixed(4)).to.be.equal('0.9167');
-      expect(Simmetrix.SmithWaterman.similarity("hallo", "halli").toFixed(4)).to.be.equal('0.8000');
-      expect(Simmetrix.SmithWaterman.similarity("hallo", "hallö").toFixed(4)).to.be.equal('0.8000');
-      expect(Simmetrix.SmithWaterman.similarity("PUNSCHIGEL", "PUNSCHIGEL / STOP DANCING IF YOU CAN!!!").toFixed(4)).to.be.equal('1.0000');
+      expect(smithWaterman.similarity("Test String1", "Test String2").toFixed(4)).to.be.equal('0.9167');
+      expect(smithWaterman.similarity("hallo", "halli").toFixed(4)).to.be.equal('0.8000');
+      expect(smithWaterman.similarity("hallo", "hallö").toFixed(4)).to.be.equal('0.8000');
+      expect(smithWaterman.similarity("PUNSCHIGEL", "PUNSCHIGEL / STOP DANCING IF YOU CAN!!!").toFixed(4)).to.be.equal('1.0000');
     }),
     it('should return 0 with undefined or empty strings', function(){
-      expect(Simmetrix.SmithWaterman.similarity("", "Test String2").toFixed(4)).to.be.equal('0.0000');
-      expect(Simmetrix.SmithWaterman.similarity(undefined, "Test String2").toFixed(4)).to.be.equal('0.0000');
-      expect(Simmetrix.SmithWaterman.similarity("Test String1", undefined).toFixed(4)).to.be.equal('0.0000');
+      expect(smithWaterman.similarity("", "Test String2").toFixed(4)).to.be.equal('0.0000');
+      expect(smithWaterman.similarity(undefined, "Test String2").toFixed(4)).to.be.equal('0.0000');
+      expect(smithWaterman.similarity("Test String1", undefined).toFixed(4)).to.be.equal('0.0000');
     })
   })
 })
